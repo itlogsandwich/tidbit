945fd9f7d3d736fadc360eb45ff8fdfbc701de67
Jan Hansen <tiujanhansen@gmail.com>
2026-03-11 21:27:29 +0800

    feat : interval and a main hero tagline focus

--- script/script.js
+++ script/script.js
@@ -1,8 +1,7 @@
-const container = document.getElementById("container");
+const mainContainer = document.querySelector("main");
 
 const tagList = [];
-
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 3; i++) {
   tagList.push(document.createElement("h1"))
 }
 
@@ -15,25 +14,68 @@ const arrayOfWords = [
   "change.",
   "cruel",
   "death",
+  "life",
 ];
 
-const randomInt = () => {
-  return Math.floor(Math.random() * arrayOfWords.length);
+const randomInt = (() => {
+  return Math.round(Math.random() * arrayOfWords.length);
+})
+const randomPositionGenerator = (val) => {
+  return [Math.round(Math.random() * Math.round(mainContainer.offsetWidth - val.offsetWidth)), Math.round(Math.random() * Math.round(mainContainer.offsetHeight - val.offsetHeight))];
 }
 
-const randomPositionGenerator = () => {
-  return Math.floor(Math.random() * container.offsetWidth);
+const randomDupe = (wordTag) => {
+
+  const dupe = wordTag.cloneNode(true);
+
+  const topVal = (() => {
+    for (let i = 0; i < wordTag.style.top.length; i++) {
+      if (wordTag.style.top[i] == "p") {
+        return wordTag.style.top.slice(0, i);
+      }
+    }
+  });
+
+  const leftVal = (() => {
+    for (let i = 0; i < wordTag.style.left.length; i++) {
+      if (wordTag.style.left[i] == "p") {
+        return wordTag.style.left.slice(0, i);
+      }
+    }
+  })
+
+  dupe.style.top = topVal() - 32 + "px";
+  dupe.style.left = leftVal() - 32 + "px";
+
+  return dupe;
 }
 
 const randomizeWordPosition = () => {
+
+  const arrH1 = mainContainer.querySelectorAll("h1");
+  if (arrH1.length >= 200) {
+    for (let h1Tag of arrH1) {
+      h1Tag.remove();
+    }
+    clearInterval(interval);
+    const statement = document.createElement("h1");
+    statement.textContent = arrayOfWords[3];
+    statement.style.top = 50 + "%";
+    statement.style.left = 50 + "%";
+    statement.style.transform = "translate(-50%, -50%)";
+    mainContainer.appendChild(statement);
+    return;
+  }
+
   for (let word of tagList) {
-    width = document.body.offsetWidth - parseInt(word.style.width);
-    height = document.body.offsetHeight - parseInt(word.style.height);
     word.textContent = arrayOfWords[randomInt()];
-    word.style.top = randomPositionGenerator() + "px";
-    word.style.left = randomPositionGenerator() + "px";
-    container.appendChild(word);
+    word.style.top = randomPositionGenerator(word)[1] + "px";
+    word.style.left = randomPositionGenerator(word)[0] + "px";
+    mainContainer.appendChild(word);
+    if (Math.round(Math.random() * 2) == randomInt()) {
+      mainContainer.appendChild(randomDupe(word))
+    }
   }
 }
 
-randomizeWordPosition();
+const interval = setInterval(randomizeWordPosition, 2);
